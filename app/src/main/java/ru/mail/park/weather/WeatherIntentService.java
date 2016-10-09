ef0868f2511558e962c51f9361c19f072bfeeae4
Temirlan Kabylbekov <temirlan.kabylbekov777@gmail.com>
2016-10-09 17:07:24 +0400

    Remove DRY in WEATHER_LOAD_ACTION field
    Remove DRY in WEATHER_LOAD_ACTION field by addressing to MainActivity public static field

--- app/src/main/java/ru/mail/park/weather/WeatherIntentService.java
+++ app/src/main/java/ru/mail/park/weather/WeatherIntentService.java
@@ -12,7 +12,7 @@ import ru.mail.weather.lib.*;
 
 public class WeatherIntentService extends IntentService {
     // TODO: set it by CityActivity class public field; don't harcode
-    private final String WEATHER_LOAD_ACTION = "ru.mail.park.weather.action.weather_load";
+//     private final String WEATHER_LOAD_ACTION = "ru.mail.park.weather.action.weather_load";
 
     public WeatherIntentService() {
         super("WeatherIntentService");
@@ -33,7 +33,7 @@ public class WeatherIntentService extends IntentService {
                 Weather weather = weatherUtils.loadWeather(city);
                 weatherStorage.saveWeather(city, weather);
 
-                sendBroadcast(new Intent(this.WEATHER_LOAD_ACTION));
+                sendBroadcast(new Intent(MainActivity.WEATHER_LOAD_ACTION));
             } catch (IOException e) {
                 Toast.makeText(this, "Ошибка при загрузке данных",
                         Toast.LENGTH_SHORT).show();
